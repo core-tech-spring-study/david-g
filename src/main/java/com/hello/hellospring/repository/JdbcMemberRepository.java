7a4aa85d453a8f7f774a53219dfdf0d6bb597a08
givxq <109785199+givxq@users.noreply.github.com>
2022-08-04 11:12:29 +0900

    jdbc repository mod

--- src/main/java/com/hello/hellospring/repository/JdbcMemberRepository.java
+++ src/main/java/com/hello/hellospring/repository/JdbcMemberRepository.java
@@ -2,12 +2,31 @@ package com.hello.hellospring.repository;
 
 import com.hello.hellospring.domain.Member;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Optional;
 
 public class JdbcMemberRepository implements MemberRepository{
+
+    private final DataSource dataSource;
+
+    public JdbcMemberRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public Member save(Member member) {
+        String sql = "insert into member(name) values(?)";
+
+        Connection conn = dataSource.getConnection();
+
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, member.getName());
+
+        pstmt.executeUpdate();
+
         return null;
     }
 
