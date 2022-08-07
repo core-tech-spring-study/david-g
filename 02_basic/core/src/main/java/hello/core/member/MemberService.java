d7db7833826ecb2fb8c6ec5daabc11c29a9b5f95
david.g <david.g@kakaopaysec.com>
2022-08-07 22:23:23 +0900

    [basic] 회원 도메인 실행과 테스트

--- 02_basic/core/src/main/java/hello/core/member/MemberService.java
+++ 02_basic/core/src/main/java/hello/core/member/MemberService.java
@@ -4,5 +4,5 @@ public interface MemberService {
 
     void join(Member member);
 
-    Member findMember(Long memberid);
+    Member findMember(Long memberId);
 }
--- 02_basic/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ 02_basic/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -6,7 +6,7 @@ public class MemberServiceImpl implements  MemberService{
 
     @Override
     public void join(Member member) {
-
+        memberRepository.save(member);
     }
 
     @Override
