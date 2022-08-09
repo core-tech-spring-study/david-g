397e1ab658ad5795616803917ea834bd55561152
david.g <david.g@kakaopaysec.com>
2022-08-09 19:55:38 +0900

    [basic] 주문과 할인 도메인 개발

---
+++ 02_basic/core/src/main/java/hello/core/discount/DiscountPolicy.java
@@ -0,0 +1,14 @@
+package hello.core.discount;
+
+import hello.core.member.Member;
+
+public interface DiscountPolicy {
+
+    /**
+     *
+     * @param member
+     * @param price
+     * @return 할인대상금액
+     */
+    int discount(Member member, int price);
+}
