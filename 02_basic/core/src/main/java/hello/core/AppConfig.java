a8b71d1dd11b9f55db9ccd953b647ebf04916af4
david.g <david.g@kakaopaysec.com>
2022-08-09 21:41:38 +0900

    [basic] AppCofing 리팩터링

--- 02_basic/core/src/main/java/hello/core/AppConfig.java
+++ 02_basic/core/src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,7 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -10,10 +12,18 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
